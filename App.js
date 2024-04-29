9e734114a0f0d1111850db5623d418ad1ec546a0
JulioCRV <julioroquevalda6@gmail.com>
2024-04-29 18:53:22 -0400

    ultimos detalles

--- App.js
+++ App.js
@@ -1,24 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 import 'react-native-gesture-handler';
-
+import React, { useEffect } from 'react';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 import ListaInvernaderos from './Components/InvernaderoLista';
 import Login from './screens/login';
 import VerConfiguraciones from './screens/ConfiguracionInventario'
 import { Ionicons } from '@expo/vector-icons';
+const Stack = createStackNavigator();
 
+const SplashScreen = ({navigation}) => {
+  // Simula un tiempo de espera antes de navegar a la página principal
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // Navegar a la página principal
+      navigation.replace('Login');
+    }, 3000); // Cambia el tiempo de espera según tus necesidades
+
+    return () => clearTimeout(timeout);
+  }, []);
+
+  return (
+    <ImageBackground
+      source={require('./assets/scesi-logo-blanco.png')} // Cambia la ruta a tu imagen de fondo
+      style={styles.container}
+      resizeMode="cover"
+    >
+      {/* Puedes agregar contenido adicional en la pantalla de inicio si es necesario */}
+    </ImageBackground>
+  );
+};
 
 export default function App() {
 
   const Stack = createStackNavigator();
-  
 
   function MyStack() {
     return (
-  
-      <Stack.Navigator>
+
+      <Stack.Navigator initialRouteName="SplashScreen">
+        <Stack.Screen name="SplashScreen" component={SplashScreen} options={{ headerShown: false }} />
+
         <Stack.Screen name='Login' component={Login}
           options={{
             title: "¡Bienvenido a AgroScesi!",
@@ -33,8 +56,8 @@ export default function App() {
             headerTitleAlign: "center",
             headerStyle: { backgroundColor: "#FCB03E" },
           }} />
-           <Stack.Screen name="VerConfig" component={VerConfiguraciones}
-         options={({ navigation }) => ({
+        <Stack.Screen name="VerConfig" component={VerConfiguraciones}
+          options={({ navigation }) => ({
             title: "CONFIGURACIONES",
             headerTintColor: "white",
             headerTitleAlign: "center",
@@ -48,7 +71,7 @@ export default function App() {
                 onPress={() => navigation.navigate('Login')}
               />
             ),
-          })}/>
+          })} />
       </Stack.Navigator>
 
     )
